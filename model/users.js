db2b0e3a914cbf470d352ce7ca0142899924132e
sri0711 <m.sathish0711@gmail.com>
2022-05-09 02:34:56 +0530

    users table done

--- model/users.js
+++ model/users.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
 
 let mongooseSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true
-    },
-    lastName:{
-        type:String,
-        required:true
-    }
-})
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	mobile: {
+		type: String,
+		required: true
+	},
+	user: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
